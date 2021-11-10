6ab271014c79c812fd52634ada6b129fccdde1b2
Bilc Denisa <bilc_denisa@yahoo.com>
2021-11-10 11:44:20 +0200

    .

--- Logic/undo.py
+++ Logic/undo.py
@@ -6,7 +6,7 @@ def undo(lista_versiuni, versiune_curenta):
     :param versiune_curenta: versiunea curenta la care ma aflu
     :return: versiunea de pe pozitia versiune_curenta-1 din lista_versiuni
     '''
-    if versiune_curenta == 0:
+    if versiune_curenta == 0 and lista_versiuni[0] == []:
         return [], 0
     versiune_curenta = versiune_curenta - 1
     return lista_versiuni[versiune_curenta], versiune_curenta
